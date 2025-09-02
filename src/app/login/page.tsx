a669609ebe6624e6cf47d714af4d25311899a96e
Dhawan Solanki <dhawansolanki2103@gmail.com>
2025-09-03 00:44:05 +0530

    Initial Commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,5 @@
+import Login from "@/components/ui/login-1";
+
+export default function LoginPage() {
+    return <Login />;
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Steps from "@/components/Steps";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+    <Header />
+    <Hero />
+    <Steps />
+    <Footer />
+    </>
+  );
+}
